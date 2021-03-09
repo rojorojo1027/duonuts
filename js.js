68bdaec80ed7edfc07ef10e044fc270a33ebc49c
rojorojo1027 <rojorojo1027@gmail.com>
2021-03-10 04:04:38 +0800

    rwd首頁

---
+++ js.js
@@ -0,0 +1,9 @@
+//navlist 轉換 RWDbtn
+// function myFunctionRWD() {
+//     var x = document.getElementById("nav");
+//     if (x.className === "nav-list") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "nav-list";
+//     }
+//   }
