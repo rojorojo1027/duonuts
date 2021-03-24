ae49df421d47b41fa4e9b5c808611c57ab44a059
rojorojo1027 <rojorojo1027@gmail.com>
2021-03-25 01:55:24 +0800

    swiper-carousel

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-/* RWD nav hamburger toggle jquery*/
+/* RWD nav hamburger toggle jquery */
 $(function(){
     // 點擊按鈕rwdbtn，選單nav-rwd縮放
     $("button.rwdbtn").on("click", function(){
@@ -7,7 +7,7 @@ $(function(){
 });
 
 
-/* index product-popup 觸發 jquery*/
+/* index product-popup 觸發 jquery */
 $("a.the_btn1").on("click", function(e){
     e.preventDefault();
     $("div.star1").fadeIn();
@@ -16,29 +16,29 @@ $("a.the_btn2").on("click", function(e){
     e.preventDefault();
     $("div.star2").fadeIn();
 });
-$("a.the_btnh1").on("click", function(e){
-    e.preventDefault();
-    $("div.hot1").fadeIn();
-});
-$("a.the_btnh2").on("click", function(e){
-    e.preventDefault();
-    $("div.hot2").fadeIn();
-});
-$("a.the_btnh3").on("click", function(e){
-    e.preventDefault();
-    $("div.hot3").fadeIn();
-});
-$("a.the_btnh4").on("click", function(e){
-    e.preventDefault();
-    $("div.hot4").fadeIn();
-});
-$("a.the_btnh5").on("click", function(e){
-    e.preventDefault();
-    $("div.hot5").fadeIn();
-});
+// $("a.the_btnh1").on("click", function(e){
+//     e.preventDefault();
+//     $("div.hot1").fadeIn();
+// });
+// $("a.the_btnh2").on("click", function(e){
+//     e.preventDefault();
+//     $("div.hot2").fadeIn();
+// });
+// $("a.the_btnh3").on("click", function(e){
+//     e.preventDefault();
+//     $("div.hot3").fadeIn();
+// });
+// $("a.the_btnh4").on("click", function(e){
+//     e.preventDefault();
+//     $("div.hot4").fadeIn();
+// });
+// $("a.the_btnh5").on("click", function(e){
+//     e.preventDefault();
+//     $("div.hot5").fadeIn();
+// });
 
 
-/*product-hover換圖 jquery*/
+/* product-hover換圖 jquery */
 $('#ori').hover(function(){ 
     $('#ori').attr('src', './img/product/ori2.png'); 
 }); 
@@ -71,7 +71,7 @@ $('#rb').hover(function(){
     $('#rb').attr('src', './img/product/rb2.png'); 
 });
 
-/*product-popup 觸發 jquery*/
+/* product-popup 觸發 jquery */
 $("a.p_btn1").on("click", function(e){
         e.preventDefault();
         $("div.p1").fadeIn();
@@ -114,53 +114,53 @@ $("a.p_btn10").on("click", function(e){
 });
 
 
-/*shoppingcart-popup 觸發 jquery*/
+/* shoppingcart-popup 觸發 jquery */
 $("a.cart").on("click", function(e){
     e.preventDefault();
     $("div.shct").fadeIn();
 });
   
-/*buybtn觸發shoppingcart-popup*/
+/* buybtn觸發shoppingcart-popup */
 $("a.buybtn").on("click", function(e){
     e.preventDefault();
     $("div.overlay").fadeOut(1000);
     $("div.shct").fadeIn(1000);
 });
 
-/*checkoutbtn觸發login-popup*/
+/* checkoutbtn觸發login-popup */
 $("button.checkoutbtn").on("click", function(e){
     e.preventDefault();
     $("div.shct").fadeOut(1000);
     $("div.loginpop").fadeIn(1000);
 });
 
-/*login-popup 觸發 jquery*/
+/* login-popup 觸發 jquery */
 $("a.login").on("click", function(e){
     e.preventDefault();
     $("div.loginpop").fadeIn();
 });
 
-/*login-popup to signup-popup*/
+/* login-popup to signup-popup */
 $("a.gosignup").on("click", function(e){
     e.preventDefault();
     $("div.loginpop").fadeOut(1000);
     $("div.signup").fadeIn(1000);
 });
 
-/*signup-popup to login-popup*/
+/* signup-popup to login-popup */
 $("a.gologin").on("click", function(e){
     e.preventDefault();
     $("div.signup").fadeOut(1000);
     $("div.loginpop").fadeIn(1000);
 });
 
-/*關閉popup*/
+/* 關閉popup */
 $("a.closebtn").on("click", function(e){
     e.preventDefault();
     $("div.overlay").fadeOut();
 });
 
-/*contact 表單資料submit驗證*/
+/* contact 表單資料submit驗證 */
 let the_form = document.getElementById("the_form");
 if (the_form){ //如果有the_form這個元素才執行
     the_form.addEventListener("submit",function(e){
@@ -201,7 +201,7 @@ if (the_form){ //如果有the_form這個元素才執行
     })
 };
 
-/*shopping-cart 全選 jquery*/
+/* shopping-cart 全選 jquery */
 $("#check_all").on("click", function(){
     $("input.itemsh").prop("checked", $(this).prop("checked"));
 });
@@ -215,7 +215,7 @@ $(document).on("click", "input.itemsh", function(){
 });
 
 
-/*product-popup prbtn nxbtn*/
+/* product-popup prbtn nxbtn */
 $("a.prbtn1").on("click", function(e){
     e.preventDefault();
     $("div.p1").fadeOut(1000);
@@ -324,4 +324,53 @@ $("a.nxbtn10").on("click", function(e){
     e.preventDefault();
     $("div.p10").fadeOut(1000);
     $("div.p1").fadeIn(1000);
+});
+
+/* index-hot-carousel swiper */
+var swiper = new Swiper('.swiper-container', {
+    // Default parameters   
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: ".rbtn",
+        prevEl: ".lbtn",
+    },
+    // Responsive breakpoints   
+    breakpoints: {  
+        575: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },       
+        767: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },    
+        991: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        }
+    }
+  });
+
+/* index-hot-carousel product-popup 觸發 */
+$("a.the_btnh1").on("click", function(e){
+    e.preventDefault();
+    $("div.hot1").fadeIn();
+});
+$("a.the_btnh2").on("click", function(e){
+    e.preventDefault();
+    $("div.hot2").fadeIn();
+});
+$("a.the_btnh3").on("click", function(e){
+    e.preventDefault();
+    $("div.hot3").fadeIn();
+});
+$("a.the_btnh4").on("click", function(e){
+    e.preventDefault();
+    $("div.hot4").fadeIn();
+});
+$("a.the_btnh5").on("click", function(e){
+    e.preventDefault();
+    $("div.hot5").fadeIn();
 });
